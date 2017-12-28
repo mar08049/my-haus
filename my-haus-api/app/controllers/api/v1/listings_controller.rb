8ccf6cf0f13373c990bca87ce28eae448e329679
github username <github email address>
2017-12-27 18:51:52 -0600

    added controller and serializer

---
+++ my-haus-api/app/controllers/api/v1/listings_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class ListingsController < ApplicationController
+
+      def index
+      end
+
+      def show
+      end
+
+      def new
+      end
+
+      def create
+      end
+
+      def destroy
+      end
+
+      private
+      set_listings
+    end
+    end
+  end
+end
--- my-haus-api/config/routes.rb
+++ my-haus-api/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    name :v1 do
+      resources :listings
+    end
+  end
 end
