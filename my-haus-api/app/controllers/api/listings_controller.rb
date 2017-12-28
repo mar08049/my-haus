fde8afd259d66bab7c7bf6674af6a911105921a1
github username <github email address>
2017-12-28 16:58:33 -0600

    began working on react components

--- my-haus-api/app/controllers/api/listings_controller.rb
+++ my-haus-api/app/controllers/api/listings_controller.rb
@@ -1,5 +1,7 @@
 class Api::ListingsController < ApplicationController
 
+  before_action :set_listing, only: [:show, :update, :destroy]
+
   def index
     render json: Listing.all
   end
@@ -18,12 +20,27 @@ class Api::ListingsController < ApplicationController
   end
 
   def update
-    listing = Listing.find_by(id: params[:id])
+    if @listing.update(listing_params)
+      render json: @listing
+    else
+      render json: { message: listing.errors }, status: 400
+    end
+  end
 
   def destroy
+    if @listing.destroy
+      render status: 204
+    else
+      render json: { message: "Unable to Destroy"}, status: 400
+    end
   end
 
   private
+
+  def set_listing
+    @listing = Listing.find_by(id: params[:id])
+  end
+
   def listing_parmas
     params.require(:listing).permit(:title, :location, :price, :description, :availability)
   end
