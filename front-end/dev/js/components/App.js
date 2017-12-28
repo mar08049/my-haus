510c4ca87a1139c1c53ca47dc905091b4ef412f3
github username <github email address>
2017-12-27 22:22:24 -0600

    updated app.js

--- front-end/dev/js/components/App.js
+++ front-end/dev/js/components/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import UserList from '../containers/user-list';
-import UserDetails from '../containers/user-detail';
+import ReactDOM from 'react-dom';
 require('../../scss/style.scss');
 
-const App = () => (
-    <div>
-        <h2>User List</h2>
-        <UserList />
-        <hr />
-        <h2>User Details</h2>
-        <UserDetails />
-    </div>
-);
+class App extends Component.React {
+  render() {
+    return(
+      <div></div>
+    )
+  );
+}
 
 export default App;
