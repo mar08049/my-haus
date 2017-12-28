00648b21106555fe13c0bd2d6e82e3f5c996d573
github username <github email address>
2017-12-28 15:00:47 -0600

    updated routes

--- my-haus-api/config/routes.rb
+++ my-haus-api/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :listings, except: [:new, :edit]
+  end
 
+  namespace :api do
+    resources :users do
+      resources :bookings
+    end
+  end
 end
