15c18ca8dda68c1d47224516f62adb63a376437b
github username <github email address>
2018-01-02 14:59:36 -0600

    added home.js component

---
+++ my-haus-client/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = (props) => {
+  return (
+    <div>
+      <h1 className="a">My Haus</h1>
+      <p className="b">Find a place to lay your head in Germany! </p>
+      <p>Thank you for visiting.</p>
+    </div>
+  );
+};
+
+export default Home;
