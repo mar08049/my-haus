8999c954345e7c58be15d443790a09d08df92235
github username <github email address>
2018-01-09 11:24:11 -0600

    working NavBar

--- my-haus-client/src/components/NavBar.js
+++ my-haus-client/src/components/NavBar.js
@@ -1,23 +1,47 @@
 import React from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {
-  return (
-    <div>
-      <Nav tabs>
-        <NavItem>
-          <NavLink tag={ Link } to="/" active>Home</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={ Link } to="/listings">Listings</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={ Link } to="/listings/new">Create Listing</NavLink>
-        </NavItem>
-      </Nav>
-    </div>
-  );
-};
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+export const NavBar = () =>
+  <div>
+    <NavLink
+      to="/"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/listings"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >View Listings</NavLink>
+    <NavLink
+      to="/listings/new"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Add Listing</NavLink>
+  </div>;
+
+export const Home = () => <h1>My Haus</h1>;
+
+export const allListings = () => <h1>Listings</h1>;
+
+export const NewListing = () => <h1>Add Listing</h1>;
 
 export default NavBar;
--- my-haus-client/src/containers/App.js
+++ my-haus-client/src/containers/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import Listings from '../components/Listings';
 import ListingService from '../services/ListingService';
 import AddListing from '../components/AddListing';
+import { NavBar, Home, NewListing, allListings } from '../components/NavBar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 
 class App extends Component {
 
   componentDidMount() {
-    // ListingService.fetchListings().then(listings =>
-    //   this.setState({ listings })
-    // )
+     ListingService.fetchListings().then(listings =>
+       this.setState({ listings })
+     )
   }
 
   addListing = listing => {
@@ -22,15 +24,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="navbar">
-          <h1> My Haus NavBar </h1>
-        </div>
+          <Router>
+            <div>
+              <NavBar />
+              <Route exact path="/" render={Home} />
+              <Route exact path="/listings" render={allListings} />
+              <Route exact path="/listings/new" render={NewListing} />
+            </div>
+          </Router>
         <div className="sidebar">
           <Listings  />
           </div>
         <div className="main-content">
           <AddListing addListing={this.addListing}/>
-
         </div>
       </div>
     );
